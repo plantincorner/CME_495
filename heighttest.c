d34ae919ea21456c79590262eea33d89b2c84e80
luke <luke.protz@usask.ca>
2016-03-25 22:00:51 -0600

    22:00

--- heighttest.c
+++ heighttest.c
@@ -44,6 +44,7 @@ int main()
 	
 	//var for math
 	float height = 0;
+	float comp_height = 0;
 	
 	//math
 	while(1){
@@ -51,9 +52,16 @@ int main()
 	BAROTEMP(&temp,&press);
 	LIDAR_LITE_(&res,fd);
 	
-	height = res*sqrt(1-(pow(sin(abs(acc_x)*(M_PI/180)),2)+pow(sin(abs(acc_y)*(M_PI/180)),2)));
 	
-	printf("height = %f cm length = %3.0d cm \n", height, res);
+	//height = (res*abs(cos(abs(acc_x)*(M_PI/180))*cos(abs(acc_y)*(M_PI/180)))) / (sqrt(pow(cos(abs(acc_x)*(M_PI/180)),2) + pow(sin(abs(acc_x)*(M_PI/180)),2) + pow(cos(abs(acc_y)*(M_PI/180)),2)));
+	
+	height = (res*cos(abs(acc_x)*(M_PI/180))*cos(abs(acc_y)*(M_PI/180))) / (sqrt(pow(cos(abs(acc_x)*(M_PI/180)),2) + pow(sin(abs(acc_x)*(M_PI/180)),2) + pow(cos(abs(acc_y)*(M_PI/180)),2)));
+	
+	comp_height = height + 0.5*height;
+	
+	printf("height = %3.0f cm comp_height = %3.0f cm length = %3.0d cm \n", height, comp_height, res);
+	//printf (" gyr_x %7.3f \t acc_x %7.3f \t gyr_y %7.3f \t acc_y %7.3f \n", gyr_x, acc_x, gyr_y, acc_y);
+	
 	}
 	//printf (" gyr_x %7.3f \t acc_x %7.3f \t gyr_y %7.3f \t acc_y %7.3f \n", gyr_x, acc_x, gyr_y, acc_y);
 	//printf("t = %f, p = %lu \n", temp, press);
