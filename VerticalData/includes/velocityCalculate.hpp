6ca58864173e0da75f85b0b4f967f850166f6893
devon <devon.haubold@usask.ca>
2016-03-26 16:58:58 -0600

    created header for lances functions header

---
+++ VerticalData/includes/velocityCalculate.hpp
@@ -0,0 +1,32 @@
+/*
+ * velocityCalculate.hpp
+ *
+ *  Created on: Mar 26, 2016
+ *      Author: devon
+ */
+
+#ifndef INCLUDES_VELOCITYCALCULATE_HPP_
+#define INCLUDES_VELOCITYCALCULATE_HPP_
+
+#include <opencv2/video/tracking.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <iostream>
+#include <ctype.h>
+#include <iomanip>
+#include <random>
+
+using namespace std;
+using namespace cv;
+
+void fourPixelAverage(InputArray imgFull, OutputArray imgReduced);
+
+float entropy(Mat seq, Size size, int index);
+
+Mat myEntropy(Mat seq, int histSize);
+
+int calculateAlmostEverythingAtOnce(double cameraElevation,double pixelHeight,int frameRate, int frameCount, double pitch, double roll, double wPitch, double wRoll, double Vx, double Vy, double Vz, double speed, double actualSpeed  );
+
+
+#endif /* INCLUDES_VELOCITYCALCULATE_HPP_ */
