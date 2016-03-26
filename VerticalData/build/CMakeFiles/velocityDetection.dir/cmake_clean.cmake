dd8b67c3ae2ff8572f63aff33ef9844a2309026f
luke <luke.protz@usask.ca>
2016-03-25 18:21:58 -0600

    new branch: software

---
+++ VerticalData/build/CMakeFiles/velocityDetection.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/velocityDetection.dir/src/velocityTracking.cpp.o"
+  "CMakeFiles/velocityDetection.dir/src/Height.cpp.o"
+  "CMakeFiles/velocityDetection.dir/src/VerticalData.cpp.o"
+  "CMakeFiles/velocityDetection.dir/src/TimedData.cpp.o"
+  "CMakeFiles/velocityDetection.dir/src/VelocityData.cpp.o"
+  "velocityDetection.pdb"
+  "velocityDetection"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/velocityDetection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
