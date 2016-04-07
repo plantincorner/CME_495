0cb95ef05430d400a15fe837a35828bd1cadc03a
devon <devon.haubold@usask.ca>
2016-04-06 22:53:01 -0600

    added documentation to sensor interface

--- Velocity_Tracking/includes/mega_sensor.h
+++ Velocity_Tracking/includes/mega_sensor.h
@@ -1,6 +1,7 @@
 /**
  * @file mega_sensor.h
  * @author Luke Protz
+ * @date March 27, 2016
  * @brief get sensor data
  */
 #ifndef MEGA_SENSOR_H
@@ -28,23 +29,22 @@
 //lidar
 
 /**
- * @brief
+ * @brief extracts data from sensors
+ * @brief detemines most accurate data based sensors being used
  *
  * @pre The Lidar is initialized
  * @pre The IMU is initialized
- * @param[out] g_x acceleration in the x direction in
- * @param[out] g_y acceleration in the y direction
- * @param[out] a_x
- * @param[out] a_y
- * @param[out] t
- * @param[out] p
- * @param[out] l
- * @param[out] l
- * @param[out] l_c
- * @param[out] b_c
- * @param[out] fd
- * @param[out]
- * @post
+ * @param[out] g_x pitch velocity in rad/s
+ * @param[out] g_y roll  velocity in rad/s
+ * @param[out] a_x pitch in radians
+ * @param[out] a_y roll in radians
+ * @param[out] t   temperature in degrees Celcius
+ * @param[out] p   pressure in pascales
+ * @param[out] l   distance from lidar in cm
+ * @param[out] l_c height calculation held when uav reaches 40 m
+ * @param[out] b_c pressure measurement held uav reaches 40 m
+ * @param[out] h   calculated height, corrected for attitude
+ * @param[out] fd  confirmation of lidar functionality, equals 1 when lidar operational
  */
 void MEGA_SENSOR(float *g_x,float *g_y,float *a_x,float *a_y,float *t,long *p,int *l,float *l_c,long *b_c,float *h,int fd);
 
--- Velocity_Tracking/includes/output.h
+++ Velocity_Tracking/includes/output.h
@@ -1,3 +1,9 @@
+/**
+*@file output.h
+*@author Luke Protz
+*@date March 25, 2016
+*@brief Interface with lidar manufacturer API
+*/
 #ifndef OUTPUT_H
 #define OUTPUT_H
 
@@ -12,8 +18,21 @@
 #include <time.h>
 #include <wiringPi.h> //added
 
+/**
+*@brief Initializes Lidar
+*@param[in] sig wake signal
+*@post lidar is operational
+*/
 void  INThandler(int sig);
 
+/**
+*@brief gathers information from IMU (accelerometer and gyroscope)
+*@param[out] gyr_x gyroscope pitch value
+*@param[out] gyr_y gyroscope roll value
+*@param[out] acc_x accelerometer pitch value
+*@param[out] acc_y accelerometer roll value
+*@post parameters contain most recent attitude measurements
+*/
 void ACCGYR(float *gyr_x, float *gyr_y, float *acc_x, float *acc_y);
 
 #endif
